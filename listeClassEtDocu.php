627510e948d8cfb70410f1e72d884adf75f0a245
Etienne Pradillon <47367782+EPradillon@users.noreply.github.com>
2020-12-16 11:46:21 +0100

    Update listeClassEtDocu.php

--- listeClassEtDocu.php
+++ listeClassEtDocu.php
@@ -68,6 +68,19 @@ class FirstTransactionStrategy implements TransactionStrategyInterface{
 
 }
 
+/**
+ * Interface de Interaction
+ * 
+ * Contrat entre l'application et le programmeur :
+ * Toute forme d'interaction devra respecter les besoins définis au travers des signatures de cette interface.
+ * 
+ * Toute demande d'interraction entre deux individu respectera "l'inversion de dépendance" (le D de SOLID)
+ * Il suffit d'appeler ce fichier en lieu et place de l'implémentation garantissant ainsi :
+ *  1. la permutabilité de multiples façon de faire intéragir deux individu. 
+ *  2. L'assurance que l'interaction respectera le contrat defini pour cette application.
+ * 
+ * @author Etienne Pradillon <epradillon@gmail.com> | Samir Founou <samir_615@live.fr>
+ */
 interface InteractionStrategyInterface {
 
 }
@@ -98,7 +111,19 @@ class PopulationFactory implements PopulationFactoryInterface{
     }
 }
 
-
+/**
+ * Interface de Distribution des richesses
+ * 
+ * Contrat entre l'application et le programmeur :
+ * Toute forme de Distribution des richesses devra respecter les besoins définis au travers des signatures de cette interface.
+ * 
+ * Tout calcul initial de richesse des individus respectera "l'inversion de dépendance" (le D de SOLID)
+ * Il suffit d'appeler ce fichier en lieu et place de l'implémentation garantissant ainsi :
+ *  1. la permutabilité de multiples façon de definir une richesse initiale au sein d'une population. 
+ *  2. L'assurance que toute distribution respectera le contrat defini pour cette application.
+ * 
+ * @author Etienne Pradillon <epradillon@gmail.com> | Samir Founou <samir_615@live.fr>
+ */
 interface DistribInterface {
 
 }
