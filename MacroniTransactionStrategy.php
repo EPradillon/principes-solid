d53aa784a66a8688789ed2d9abbc7c0c8f2fd739
etienne.prad <epradillon@gmail.com>
2020-12-19 16:47:52 +0100

    seconde version de transaction

---
+++ MacroniTransactionStrategy.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+require_once("./TransactionStrategyInterface.php");
+
+class MacroniTransactionStrategy implements TransactionStrategyInterface{
+
+    /**
+     * Effectue une transaction entre 2 individus
+     *
+     * @param Array $population 
+     * @return Array
+     */
+    public function makeTransaction(Array $selectedIndividuals): Array
+    {
+        $sharedWealth = array_sum ($selectedIndividuals);
+        $firstGuyMoney = $selectedIndividuals[array_keys($selectedIndividuals)[0]];
+        $secondGuyMoney = $selectedIndividuals[array_keys($selectedIndividuals)[1]];
+        // Le plus riche des deux individus récupère la totalité.
+        if ($firstGuyMoney < $secondGuyMoney) {
+            $selectedIndividuals[array_keys($selectedIndividuals)[0]] = $sharedWealth;
+            $selectedIndividuals[array_keys($selectedIndividuals)[1]] = 0;
+        } else {
+            $selectedIndividuals[array_keys($selectedIndividuals)[1]] = $sharedWealth;
+            $selectedIndividuals[array_keys($selectedIndividuals)[0]] = 0;
+        }
+        return $selectedIndividuals;
+    }
+}
--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 ## Php :
 
+Depuis php 7.4 il est possible de choisir entre un typage fort ou faible.
 ```php
 declare(strict_types = 1);
 ```
+Cette instruction impose au code du fichier un typage fort.
